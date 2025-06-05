c4a8c8c0fb5daca527e7efab4887b03f0d70dcdd
Din Islam <86012677+Dinislam360@users.noreply.github.com>
2025-06-05 16:29:21 +0600

    Update Category.route.js

--- api/routes/Category.route.js
+++ api/routes/Category.route.js
@@ -1,22 +1,14 @@
-import express from 'express';
-import { 
-    addCategory, 
-    deleteCategory, 
-    getAllCategory, 
-    showCategory, 
-    updateCategory 
-} from '../controllers/Category.controller.js';
-import { onlyadmin } from '../middleware/onlyadmin.js';
+import express from 'express'
+import { addCategory, deleteCategory, getAllCategory, showCategory, updateCategory } from '../controllers/Category.controller.js'
+import { onlyadmin } from '../middleware/onlyadmin.js'
 
-const CategoryRoute = express.Router();
+const CategoryRoute = express.Router()
 
-// Admin-only routes
-CategoryRoute.post('/add', onlyadmin, addCategory);
-CategoryRoute.put('/update/:categoryid', onlyadmin, updateCategory);
-CategoryRoute.get('/show/:categoryid', onlyadmin, showCategory);
-CategoryRoute.delete('/delete/:categoryid', onlyadmin, deleteCategory);
+CategoryRoute.post('/add', onlyadmin, addCategory)
+CategoryRoute.put('/update/:categoryid', onlyadmin, updateCategory)
+CategoryRoute.get('/show/:categoryid', onlyadmin, showCategory)
+CategoryRoute.delete('/delete/:categoryid', onlyadmin, deleteCategory)
+CategoryRoute.get('/all-category', getAllCategory)
 
-// Public route
-CategoryRoute.get('/all-category', getAllCategory);
 
-export default CategoryRoute;
+export default CategoryRoute
